2cd8de6255e6543fd434499d741d0e8bb48def41
Max Bonas <113984950+MaxBonas@users.noreply.github.com>
2025-06-15 01:06:41 +0200

    Launch translation asynchronously

--- src/main/java/traductor/Main.java
+++ src/main/java/traductor/Main.java
@@ -1,6 +1,7 @@
 package traductor;
 
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,15 @@ public class Main {
             SpeechTranslatorService translatorService = new SpeechTranslatorService(targetLanguage);
             TranslatorAppView view = new TranslatorAppView(translatorService);
             view.setVisible(true);
+
+            SwingWorker<Void, Void> worker = new SwingWorker<>() {
+                @Override
+                protected Void doInBackground() {
+                    translatorService.startTranslation();
+                    return null;
+                }
+            };
+            worker.execute();
         });
     }
 }
